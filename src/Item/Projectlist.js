1ea47cde5dec0c5671b0532a377adaf0672060b8
ArshalanQuasain <arshalanquasain.01@gmail.com>
2024-08-08 23:26:59 +0530

    added currency project

--- src/Item/Projectlist.js
+++ src/Item/Projectlist.js
@@ -1,6 +1,7 @@
 import Todo from "../Image/Todo.png"
 import Blog from "../Image/Blog.png"
 import notesApp from "../Image/notes-app.png"
+import currency from "../Image/currency-changer.jpg"
 export const ProjectList = [
     {
       name: "Notes-App",
@@ -15,6 +16,13 @@ export const ProjectList = [
       skills: "React-js,javascript,HTML,CSS,appwrite",
       Github: "https://github.com/ArshalanQuasain/Bloging-App",
       Link:"https://bloging-app-six.vercel.app/"
+    },
+    {
+      name: "Currency_Changer",
+      image: currency,
+      skills: "React-js,javascript,HTML,CSS ,API",
+      Github: "https://github.com/ArshalanQuasain/currencyChnager",
+      Link: "https://currency-chnager.vercel.app/"
     },
     {
       name: "Todo Manager",
